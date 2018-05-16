7e55ddd5c8f907918bb854602f9237700edfc36f
otaviosumi <otaviosumi@hotmail.com>
2018-05-15 22:04:21 -0300

    +login

--- PythonApp/app.py
+++ PythonApp/app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request , url_for, redirect
+from flask import Flask, render_template, request , url_for, redirect, flash, session, abort
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+	if not session.get('logged_in'):
+		return render_template('showsignup.html')
+	else:
+		return render_template('index.html')
 
-@app.route('/showSignUp')
+@app.route('/showsignup', methods=['POST', 'GET'])
 def showSignUp():
-    return render_template('showsignup.html')
+	if request.form['password']=='password' and request.form['username'] == 'user':
+		session['logged_in'] = True
+	else:
+		flash('Senha incorreta!!!')
+	return main()
 
 if __name__ == "__main__":
-	app.run()
+	app.secret_key = os.urandom(12)
+	app.run(debug = True, host='0.0.0.0', port=4000)
 
