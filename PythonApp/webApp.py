cb6c6c27f146c826e0a9bd5574ba7affff879f53
otaviosumi <otaviosumi@hotmail.com>
2018-06-11 14:38:49 -0300

    addin styles

--- PythonApp/webApp.py
+++ PythonApp/webApp.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template, request , url_for, redirect, flash, se
 import os
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template('home.html')
+user = {'username' : 'Bob'}
 
+vacinas = [
+	{
+		'tipo' : 'caxumba', 'data' : '21/03/2005', 'lote' : '0111944'
+	},
+	{
+		'tipo' : 'febre amarela', 'data' : '19/08/2008', 'lote' : '0111946'
+	}
+]
+
+@app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html')
+	return render_template('home.html', vacinas=vacinas, user=user)
 
 @app.route("/showlogin")
 def login():
@@ -17,9 +25,6 @@ def login():
 @app.route("/showlogout")
 def logout():
 	return render_template('showlogout.html')
-
-
-
 
 
 if __name__ == "__main__":
