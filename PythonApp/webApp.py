5f7bf1ab834ab01a787e596ae69822d60642b1ba
otaviosumi <otaviosumi@hotmail.com>
2018-06-16 17:46:20 -0300

    +Vaccines list on userDB

--- PythonApp/webApp.py
+++ PythonApp/webApp.py
@@ -3,7 +3,7 @@ import os
 import datetime
 from pymongo import MongoClient
 
-#Starting and creating DB
+#Starting and creating DB###################################################################
 client = MongoClient('localhost', 27017)
 db = client['database']
 
@@ -24,14 +24,25 @@ allergies = [
 		}
 ]
 
+vaccines = [
+	{
+		'tipo' : 'caxumba', 'data' : '21/03/2005', 'lote' : '0111944'
+	},
+	{
+		'tipo' : 'febre amarela', 'data' : '19/08/2008', 'lote' : '0111946'
+	}
+]
+
 user = {
 		'_id' : 1,
 		'username' : 'Bob',
+		'sus' : 3928840,
 		'age' : '26',
 		'bloodType' : 'A+',
 		'birthday' : '24/02/1992',
 		'dateInserted' : datetime.datetime.now(),
-		'allergies' : allergies
+		'allergies' : allergies,
+		'vaccines' : vaccines
 		}
 
 usuarios = db.users
@@ -44,25 +55,26 @@ else:
 	print('User inserted')
 	usuarios.insert_one(user)
 
+def findUserBySUS(susNumber):
+	return usuarios.find_one({'sus' : susNumber})
+
+def getUserVaccines(susNumber):
+	return usuarios.find_one({'sus' : susNumber})['vaccines']
+
+#End of DB start code #DB###################################################################
+
 username = 'Bob'
 
 app = Flask(__name__)
 
 
 
-vacinas = [
-	{
-		'tipo' : 'caxumba', 'data' : '21/03/2005', 'lote' : '0111944'
-	},
-	{
-		'tipo' : 'febre amarela', 'data' : '19/08/2008', 'lote' : '0111946'
-	}
-]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html', vacinas=vacinas, user=usuarios.find_one({'username' : username}))
+	return render_template('home.html', vacinas=getUserVaccines(3928840), user=findUserBySUS(3928840))
 
 @app.route("/showlogin")
 def login():
